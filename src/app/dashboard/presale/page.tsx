257951d11264099142ccfca1bd417d324ed15b19
Aqua Marine <aquamarine@ruby.exchange>
2024-06-30 14:33:40 +0700

    change ido sym logo

--- src/app/dashboard/presale/page.tsx
+++ src/app/dashboard/presale/page.tsx
@@ -10,18 +10,20 @@ import SpinImage from "@/app/Components/SpinImage";
 import Presale from "@/app/Components/Presale";
 
 import { usePresale } from "@/app/Hooks/usePresale";
+import { findTokenFromAddress } from "@/app/Utils/findTokens";
 
 const Home = ({ children, params }: any) => {
   const { address, isConnected, chain } = useAccount();
 
   const { data: presaleTokenAddress } = usePresale("currentTokenSale");
 
+  const sym = findTokenFromAddress(presaleTokenAddress);
+
+  console.log("Presale: Home:  Presale token :", sym);
+
   return (
     <main className={styles_container.container}>
       <h1 className={styles.text_title}>IDO Platform </h1>
-
-      <span className={styles.text_border_bottom}> Initial DEX Offering</span>
-      <SpinImage imageUrl="/AQUA.png" />
 
       {!address || !isConnected || (chain && chain.id !== CHAIN.id) ? (
         <div className="button_back">
@@ -33,7 +35,18 @@ const Home = ({ children, params }: any) => {
         </div>
       ) : (
         <div>
-          <Presale props={presaleTokenAddress}></Presale>
+          {presaleTokenAddress ? (
+            <span>
+              {" "}
+              <span className={styles_container.container}>
+                {" "}
+                <SpinImage imageUrl={`${sym.logo}`} />{" "}
+              </span>
+              <Presale props={presaleTokenAddress}></Presale>{" "}
+            </span>
+          ) : (
+            <span> Create IDO </span>
+          )}
         </div>
       )}
     </main>
